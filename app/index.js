6f3f1c244e460e00f01a5e28b6950852b1bd2144
Shivam Gupta <guptashivam9893@gmail.com>
2023-04-13 00:28:32 +0530

    COmpleted the React Native Mobile App

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { View, Text, SafeAreaView, ScrollView } from 'react-native'
 import { Stack, useRouter } from 'expo-router'
 
+
 import { COLORS, images, icons, SIZES } from '../constants'
 import {
   Nearbyjobs,
